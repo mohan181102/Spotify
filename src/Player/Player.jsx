8f052270e295063cec16bc1970a9c0c900b76d69
manmohan <manmohan33chandrakar@gmail.com>
2024-02-22 19:11:49 +0530

    change 22/02/24

--- src/Player/Player.jsx
+++ src/Player/Player.jsx
@@ -3,18 +3,48 @@ import "./Player.css";
 
 function Player({ src }) {
   const audio = useRef(null);
+  const [value, setvalue] = useState(null);
+  const [playstate, setplaystate] = useState(false);
+
+  // CREATE SONGSTATUS BAR
+
+  setTimeout(() => {
+    if (audio.current.currentTime <= audio.current.duration) {
+      setvalue(
+        (Math.floor(audio.current.currentTime) * 100) /
+          Math.floor(audio.current.duration)
+      );
+    } else {
+      return;
+    }
+  }, 1000);
+
+  // LOAD EVERY SONG SELECTION
 
   useEffect(() => {
     audio.current.load();
+    setplaystate(true);
     if (src) {
       if (src.preview_url == undefined || null) {
         alert("preview now present");
       }
     }
-    console.log(src);
   }, [src]);
 
-  // useEffect(() => {});
+  // MANAGE STATE ON PLAY AND PAUSE BUTTON
+
+  function statemanage() {
+    if (audio != null && playstate == false) {
+      audio.current.play();
+      setplaystate(true);
+    }
+
+    if (audio != null && playstate == true) {
+      audio.current.pause();
+      setplaystate(false);
+    }
+  }
+
   return (
     <div id="player">
       <img
@@ -27,11 +57,17 @@ function Player({ src }) {
             : ""
         }
       />
-      {/* <input value={} type="range" /> */}
+
+      <div>
+        <input type="range" value={value} />
+        <button onClick={() => statemanage()}>
+          {playstate ? "Pause" : "Play"}
+        </button>
+      </div>
+
       <audio
         id="audio"
         ref={audio}
-        controls
         src={src ? src.preview_url : ""}
         autoPlay
       ></audio>
